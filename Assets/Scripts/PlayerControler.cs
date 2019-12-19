d6a09505efc256129c389c85f9b2195782762c90
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-12-19 01:36:53 +0100

    'did_movement_chara'

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public int speed = 7, jump = 300;
+    public bool isJumping = false;
+
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * h * speed * Time.deltaTime);
+
+        if (h > 0) GetComponent<SpriteRenderer>().flipX = false;
+        if (h < 0) GetComponent<SpriteRenderer>().flipX = true;
+
+    }
+
+    void FixedUpdate()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * jump * Time.deltaTime;
+        }
+    }
+}
