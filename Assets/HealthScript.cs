f591cfbbc1d14331082bfe500c8495a7a44b0881
arsandamus <55235077+arsandamus@users.noreply.github.com>
2019-12-19 23:50:03 +0100

    add Santa with movements and candy

---
+++ Assets/HealthScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    public int hp = 1;
+    public bool isEnemy = true;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Est-ce un tir ?
+        ShotSprite shot = collider.gameObject.GetComponent<ShotSprite>();
+        if (shot != null)
+        {
+            // Tir allié
+            if (shot.isEnemyShot != isEnemy)
+            {
+                hp -= shot.damage;
+
+                // Destruction du projectile
+                // On détruit toujours le gameObject associé
+                // sinon c'est le script qui serait détruit avec ""this""
+                Destroy(shot.gameObject);
+
+                if (hp <= 0)
+                {
+                    // Destruction !
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
