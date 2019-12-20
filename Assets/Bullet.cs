eb772bbf782dc41f12268e3faef749f18ef5f3c6
TlZeyn <jacqueline.nguyen.41@gmail.com>
2019-12-20 09:50:27 +0100

    'Added_boss'

---
+++ Assets/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+
+{
+    float moveSpeed = 7f;
+    Rigidbody2D rb;
+    public int damage = 1;
+
+    Target target;
+    Vector2 moveDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindObjectOfType<Target>();
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+        Destroy(gameObject, 3f);
+    }
+
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+       if (col.gameObject.name.Equals("Character"))
+        {
+            Debug.Log("Hit");
+            
+        }
+    }
+}
---
+++ Assets/Chara_health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chara_health : MonoBehaviour
+{
+    public int hp = 5;
+    
+
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        // Est-ce un tir ?
+        Bullet shot = collider.gameObject.GetComponent<Bullet>();
+        if (shot != null)
+        {
+
+            hp -= shot.damage;
+            Debug.Log(shot);
+
+            // Destruction du projectile
+            // On détruit toujours le gameObject associé
+            // sinon c'est le script qui serait détruit avec ""this""
+            Destroy(shot.gameObject);
+
+            if (hp <= 0)
+            {
+                // Destruction !
+                Destroy(gameObject);
+            }
+
+        }
+    }
+}
